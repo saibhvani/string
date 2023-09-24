7afbd59fcb3a7a69baf83f9f9d2b8d1758d6c3a1
saibhvani <145922824+saibhvani@users.noreply.github.com>
2023-09-24 22:29:09 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,57 @@
+#string conversion
+#string datatype to another datatype
+
+#implict
+a = "Welcome to VKC"
+print(a)
+print(type(a))
+
+#explict
+a = str("saibhavani")
+print(a)
+print(type(a))
+
+#data type /variable annotation
+a : str = "VKC"
+print(a)
+print(type(a))
+
+#binary
+a = str(0b1001)
+print(a)
+print(type(a))
+
+#octal
+a = str(0b1001)
+print(a)
+print(type(a))
+
+#hexa
+a = str(0o56)
+print(a)
+print(type(a))
+
+#boolean
+a = str(True)
+print(a)
+print(type(a))
+
+#boolean conversation
+a = bool(0)
+print(a)
+print(type(a))
+
+#hexa conversion
+a = hex(0x45)
+print(a)
+print(type(a))
+
+#octal conversion
+a = oct(0o35)
+print(a)
+print(type(a))
+
+#binary conversion
+a = bin(0b1001)
+print(a)
+print(type(a))
